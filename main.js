a02b4f81e6bf167c28db2f0b240fc1e8cdabdf25
Fourseasons designworks <chiharyth429@gmail.com>
2026-05-04 23:54:08 +0900

    20260504-2

--- main.js
+++ main.js
@@ -17,3 +17,46 @@ const observer = new IntersectionObserver(
 );
 
 targets.forEach((el) => observer.observe(el));
+
+// ハンバーガーメニュー
+$(".openbtn").click(function () {
+  $(this).toggleClass("active");
+});
+
+const slides = document.querySelector(".slides");
+const items = document.querySelectorAll(".slides li");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const dotsContainer = document.querySelector(".dots");
+
+let index = 0;
+
+// ドット生成
+items.forEach((_, i) => {
+  const dot = document.createElement("button");
+
+  if (i === 0) dot.classList.add("active");
+
+  dot.addEventListener("click", () => {
+    index = i;
+    updateSlide();
+  });
+
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dots button");
+
+// スライド更新
+function updateSlide() {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+
+  // ドット更新
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + items.length) % items.length;
+  updateSlide();
+});
